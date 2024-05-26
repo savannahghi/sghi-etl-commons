4f26bc9f2b24c2db1acff289c7545b0dc68d0203
Kennedy Kori <kennedykori47@gmail.com>
2024-05-26 17:24:57 +0300

    feat(utils): add a workflow runner utility
    Add `sghi.etl.commons.utils.run_workflow`, a utility function that
    executes ETL Workflows. This function accepts a factory function that
    supplies an SGHI ETL `WorkflowDefinition` instance, it then invokes the
    function to get the `WorkflowDefinition`/workflow and then executes it.

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,9 @@ exclude_patterns = []
 nitpicky = True
 
 nitpick_ignore = [
+    ("py:attr", "sghi.etl.core.WorkflowDefinition.processor_factory"),  # docs aren't published yet
+    ("py:attr", "sghi.etl.core.WorkflowDefinition.sink_factory"),  # docs aren't published yet
+    ("py:attr", "sghi.etl.core.WorkflowDefinition.source_factory"),  # docs aren't published yet
     ("py:class", "_RDT"),  # private type annotations
     ("py:class", "_PDT"),  # private type annotations
     ("py:class", "Executor"),  # sphinx can't find it
@@ -85,6 +88,7 @@ nitpick_ignore = [
     ("py:exc", "ResourceDisposedError"),  # docs aren't published yet
     ("py:exc", "sghi.disposable.ResourceDisposedError"),  # docs aren't published yet
     ("py:func", "sghi.disposable.not_disposed"),  # docs aren't published yet
+    ("py:meth", "sghi.etl.core.Source.draw"),  # docs aren't published yet
     ("py:obj", "sghi.etl.commons.processors._PDT"),  # private type annotations
     ("py:obj", "sghi.etl.commons.processors._RDT"),  # private type annotations
     ("py:obj", "sghi.etl.commons.sinks._PDT"),  # private type annotations
