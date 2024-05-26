6274a31ec774e5fd720938360702af8e44994d63
Kennedy Kori <kennedykori47@gmail.com>
2024-05-26 16:00:15 +0300

    feat(sinks): add a `ScatterSink` (#21)
    Add `sghi.etl.commons.sink.ScatterSink`, a `Sink` that drains (the same)
    processed data to multiple other sinks concurrently.

--- src/sghi/etl/commons/__init__.py
+++ src/sghi/etl/commons/__init__.py
@@ -8,7 +8,7 @@ from .processors import (
     pipe_processors,
     processor,
 )
-from .sinks import NullSink, SplitSink, sink
+from .sinks import NullSink, ScatterSink, SplitSink, sink
 from .sources import GatherSource, source
 from .utils import fail_fast, fail_fast_factory, ignored_failed
 from .workflow_definitions import SimpleWorkflowDefinition
@@ -20,6 +20,7 @@ __all__ = [
     "ProcessorPipe",
     "SimpleWorkflowDefinition",
     "ScatterGatherProcessor",
+    "ScatterSink",
     "SplitGatherProcessor",
     "SplitSink",
     "fail_fast",
